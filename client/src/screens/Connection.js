b212962e927138773405af4501732c77c45e1498
Roy Buyse <rbuyse@me.com>
2018-11-13 19:28:38 -0900

    update added video player

--- client/src/screens/Connection.js
+++ client/src/screens/Connection.js
@@ -1,8 +1,40 @@
-import React from 'react';
+// import React from 'react';
 import Header from '../components/header_footer/Header';
 
+import React, { Component } from 'react'
+import YouTube from 'react-youtube';
 
+import './Connection.css';
+//www.youtube.com/watch?v=HvZU41cFJR0&t=4s
 //const Home = () => <Header title= "Home" />;
 const Connection = () => <div style={{backgroundColor: 'blue'}} />;
 
-export default Connection;
+
+
+ class ReactYouTubeExample extends Component {
+    
+    videoOnReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo()
+        console.log(event.target)
+      }
+    render() {
+        const opts = {
+          height: '390',
+          width: '640',
+          playerVars: { // https://developers.google.com/youtube/player_parameters
+            autoplay: 1
+          }
+        };
+        const {videoId} = this.props
+        return (
+          <YouTube
+            videoId={'HvZU41cFJR0'}
+            opts={opts}
+            onReady={this.videoOnReady}
+          />
+        );
+      }
+    }
+    export default ReactYouTubeExample
+// export default Connection;
