7c75345b9c1b14a1010e21189da9122a456ff797
hendrixjt <joshuathendrix@gmail.com>
2018-12-19 23:13:14 -0700

    adjusted listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,17 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
-    const listings = sequelize.define(
-      'listings',
-      {
-        ListingId: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true
-        },
-        Company: DataTypes.STRING,
-        Position: DataTypes.STRING,
-        Location: DataTypes.STRING,
-        
+  const listings = sequelize.define(
+    'listings',
+    {
+      ListingId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
       },
-    );
-    return listings;
-  };
+      Company: DataTypes.STRING,
+      Position: DataTypes.STRING,
+      Location: DataTypes.STRING,
+      Email: DataTypes.STRING,
+      Phone: DataTypes.STRING,
+    },
+  );
+  return listings;
+};
